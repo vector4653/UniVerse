07fb79e2f8f4aa7871a6aa560d3ae41cff3473cc
Ritvik Nambiar <ritviknambiar09@gmail.com>
2025-11-30 10:19:54 +0530

    Backend setup for CampusConnect

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
-
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect("mongodb://localhost:27017/monprojectWee");
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require('express');
 const cors = require("cors");
 require("dotenv").config();
-const connectDB = require("./config/db");
-const authRoutes = require("./routes/authRoutes");
+const connectDB = require("../src/config/db");
+const authRoutes = require("../src/routes/authRoutes");
 
 const app = express();
 
